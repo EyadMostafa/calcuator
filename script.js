200ac32466b9a8dc35e7bec0b6245d4b7ca0c1f6
EyadMostafa <79060024+EyadMostafa@users.noreply.github.com>
2023-03-18 20:47:58 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+"use strict";
+const display = document.querySelector('input[name="display"]');
+const buttons = document.querySelectorAll('input[type="button"]');
+
+const type = function (button) {
+  button.addEventListener(
+    "click",
+    () =>
+      `${(display.value += `${
+        typeof button.defaultValue === "number"
+          ? new Big(button.defaultValue)
+          : button.defaultValue
+      }`)}`
+  );
+};
+
+const reset = function (button) {
+  button.addEventListener("click", () => `${(display.value = "")}`);
+};
+
+const de = function (button) {
+  button.addEventListener(
+    "click",
+    () => `${(display.value = display.value.slice(0, -1))}`
+  );
+};
+
+const calculate = function (button) {
+  button.addEventListener(
+    "click",
+    () => (display.value = Big(eval(display.value)).round(2).valueOf())
+  );
+};
+
+buttons.forEach((button) => {
+  if (
+    button.defaultValue !== "AC" &&
+    button.defaultValue !== "DE" &&
+    button.defaultValue !== "="
+  )
+    type(button);
+  else if (button.defaultValue === "AC") reset(button);
+  else if (button.defaultValue === "DE") de(button);
+  else if (button.defaultValue === "=") calculate(button);
+});
